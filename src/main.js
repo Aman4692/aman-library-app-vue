7cf356615d3127c5aa502da41aafaecaea9799fb
Aman Gupta <agup0045@student.monash.edu>
2024-08-07 11:30:05 +1000

    completed activities

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+
+import './styles.css'
+// import './assets/main.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 createApp(App).mount('#app')
