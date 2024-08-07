48b61e11f1eaa6aca587d49e88d92f82689d7b1e
Aman Gupta <agup0045@student.monash.edu>
2024-08-07 10:20:59 +1000

    updated App

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,24 @@
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
 import JsonVue from './components/JSON.vue'
+import LoginForm from './components/LoginForm.vue'
 </script>
 
 <template>
-  <header>
+  <LoginForm />
+  <!-- <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <!-- <HelloWorld msg="You did it!" /> -->
-      <JsonVue />
-    </div>
-  </header>
+      <!-- <JsonVue /> -->
+    <!-- </div> -->
+  <!-- </header> -->
 
-  <main>
+  <!-- <main> -->
     
     
-  </main>
+  <!-- </main> -->
 
   
 </template>
