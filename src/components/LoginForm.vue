ac8d03a66f9695472d1abab00daf969a822165bb
Aman Gupta <agup0045@student.monash.edu>
2024-08-07 10:28:48 +1000

    added form fields

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,6 +1,27 @@
 <template>
     <div>
         <h1>User Information / Credentials</h1>
+
+        <form action="">
+            <label for="username">Username:</label><br>
+            <input type="text" id="username" name="username"><br>
+
+            <label for="password">Password:</label><br>
+            <input type="password" id="password" name="password"><br>
+
+            <label for="isAustralian">Australian Resident?</label><br>
+            <input type="checkbox" id="isAustralian" name="isAustralian"><br>
+
+            <label for="reason">Reason For Joining:</label><br>
+            <input type="text" id="username" name="username"><br>
+
+            <label for="gender">Gender</label><br>
+            <select id="gender">
+                <option value="female">Female</option>
+                <option value="male">Male</option>
+                <option value="other">Other</option>
+            </select>
+        </form>
     </div>
 </template>
 
@@ -8,5 +29,4 @@
 
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
