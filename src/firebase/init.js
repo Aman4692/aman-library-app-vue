5f2e9bde2e90b7407d424011ad5254be0976ee40
Aman Gupta <agup0045@student.monash.edu>
2024-10-17 14:18:07 +1100

    added env

---
+++ src/firebase/init.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_APP_FIREBASE_KEY,
+    authDomain: "week7-aman.firebaseapp.com",
+    projectId: "week7-aman",
+    storageBucket: "week7-aman.appspot.com",
+    messagingSenderId: "420941906846",
+    appId: "1:420941906846:web:fff4ff231920193b890c8f"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+const db = getFirestore()
+export default db
